04bb8c479af12a0478ff5c9bca2a7db427253a58
alexpryp <alexpryp@gmail.com>
2019-03-17 19:51:39 +0200

    Added second quick sort implementation

--- quick_sort.js
+++ quick_sort.js
@@ -2,23 +2,76 @@
 
 //Quick Sort
 function QuickSort(arr) {
-    if (arr.length == 0) {
-        return [];
+if (arr.length == 0) {
+    return [];
+}
+
+let a = [], b = [], p = arr[0];
+
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < p) {
+        a[a.length] = arr[i];
+    } else {
+        b[b.length] = arr[i];
     }
+}
 
-    let a = [], b = [], p = arr[0];
+return QuickSort(a).concat(p, QuickSort(b));
+}
 
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < p) {
-            a[a.length] = arr[i];
-        } else {
-            b[b.length] = arr[i];
+//Quick Sort 2
+function swap(items, firstIndex, secondIndex) {
+    const temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, left, right) {
+    let pivot = items[Math.floor((right + left) / 2)];
+    let i = left;
+    let j = right;
+
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+
+        while (items[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
         }
     }
 
-    return QuickSort(a).concat(p, QuickSort(b));
+    return i;
+}
+
+function quickSort2(items, left, right) {
+    let index;
+
+    if (items.length > 1) {
+        index = partition(items, left, right);
+
+        if (left < index - 1) {
+            quickSort2(items, left, index - 1);
+        }
+
+        if (index < right) {
+            quickSort2(items, index, right);
+        }
+    }
+
+    return items;
 }
 
 //Test
-console.log(QuickSort([5, 4, 6, 3, 7, 2, 8, 1, 9, 10, 11, 19, 20, 12, 18, 13, 17, 14, 16, 15]));
+let item = [5, 4, 6, 3, 7, 2, 8, 1, 9, 10, 11, 19, 20, 12, 18, 13, 17, 14, 16, 15];
+console.log(QuickSort(item));
+
+console.log(quickSort2(item, 0, item.length - 1));
